7482e979119737fb202fd195f93e8400e3dada4b
khouyyi1 <anasskhouyyi@gmail.com>
2023-06-04 17:54:27 +0200

    commit2

--- app/src/main/java/com/anasskhouyyi/naturecollection/MainActivity.kt
+++ app/src/main/java/com/anasskhouyyi/naturecollection/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     NatureCollectionTheme {
-        Greeting("Android")
+        Greeting("Anass")
     }
 }
